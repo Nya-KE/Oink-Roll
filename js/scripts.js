4c48d9dbdd45a11beb47218b2bed5589316dff45
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-29 23:01:57 +0300

    adds fade in for player details section

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Business Logic
+/* To include code which: 1) takes entries 2) adds cumulative entries and prepares for display */
+
+//User Interface Logic
+/* To include code which: 1)Which toggles and hide/shows various parts 2) takes entries from inputs (in future, users could also enter parameter for their game) 3) displays calculated score from BL and shows refresh  */
+$(document).ready(function(){
+  $(".start").click(function(){
+    $(".playdetails").fadeIn();
+  })
+
+})
