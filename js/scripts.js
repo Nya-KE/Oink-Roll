e1663325da059c39e09abe569d22e711846d249d
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-30 04:10:55 +0300

    adds jumbotron to story and non-functional nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 //Business Logic
 /* To include code which: 1) takes entries 2) adds cumulative entries and prepares for display */
+$('a.nav-item.Story').tab('#story') // Select tab by name
+$('#myTab li:first-child a').tab('show') // Select first tab
+$('#myTab li:last-child a').tab('show') // Select last tab
+$('#myTab li:nth-child(3) a').tab('show') // Select third tab
 
+function(){
+
+}
 //User Interface Logic
 /* To include code which: 1)Which toggles and hide/shows various parts 2) takes entries from inputs (in future, users could also enter parameter for their game) 3) displays calculated score from BL and shows refresh  */
 $(document).ready(function(){
   $(".start").click(function(){
     $(".playdetails").fadeIn();
-  })
+  });
+  $("input.player1").append(var Piggy1)
+
 
 })
